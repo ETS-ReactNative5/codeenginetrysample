e7d5f99b7fdaef83fc885c4e0626d045debfa085
Rashmi B R <52536794+testrashmi@users.noreply.github.com>
2022-03-12 20:28:12 +0530

    Delete src/services/hotel-v1-python directory

--- src/services/hotel-v1-python/app/errors/tag_not_found.py
+++
@@ -1,5 +0,0 @@
-class TagNotFoundException(Exception):
-    def __init__(self, tag):
-        super().__init__("Tag {0} does not exist.".format(tag))
-        self.errors = "Tag Not Found Exception"
-        print(self.errors)
